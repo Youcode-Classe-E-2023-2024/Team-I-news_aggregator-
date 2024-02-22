8cdd75cd3f3523a6e310d672d384b0def98bbfe6
imadovetch <ibouderoua63@gmail.com>
2024-02-22 17:16:06 +0100

    add navbar dynamic componment

--- resources/views/Shared/components/navbar.blade.php
+++ resources/views/Shared/components/navbar.blade.php
@@ -3,9 +3,9 @@
 @props(['links'])
 
 
-<nav class="relative px-4 py-4 mt-1 flex justify-between border-b-slate-300 bg-black items-center ">
+<nav style="box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;" class="relative px-4 py-4  h-16 mt-0.5 flex justify-between border-b-black bg-black  items-center ">
 		<a class="text-3xl font-bold leading-none" href="#">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse w-36 h-12">
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse w-40 h-12">
             <img src="logonews.png" class="h-12" alt="Logo">
         </a>
 		</a>
@@ -20,11 +20,11 @@
 		<ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
             @foreach($links as $key => $value)
                 <li>
-                    <a class="text-sm text-gray-400 hover:text-gray-500" href="{{ $value }}">
+                    <a class=" text-base font-mono text-white hover:text-gray-300 hover:scale-125" href="{{ $value }}">
                         {{ $key }}
                     </a>
                 </li>
-                <li class="text-gray-300">
+                <li class="text-white font-mono">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
                     </svg>
