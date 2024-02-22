6cca71d859dd066e18637b2ae5712990996a3aac
imadovetch <ibouderoua63@gmail.com>
2024-02-22 14:31:02 +0100

    navbar dynamic

--- resources/views/Shared/components/navbar.blade.php
+++ resources/views/Shared/components/navbar.blade.php
@@ -2,10 +2,36 @@
 
 @props(['links'])
 
-<nav>
-    <ul>
-        @foreach($links as $key => $value)
-            <li><a  href="{{ $value }}">{{ $key }}</a></li>
-        @endforeach
-    </ul>
-</nav>
+
+<nav class="relative px-4 py-4 mt-1 flex justify-between border-b-slate-300 bg-black items-center ">
+		<a class="text-3xl font-bold leading-none" href="#">
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse w-36 h-12">
+            <img src="logonews.png" class="h-12" alt="Logo">
+        </a>
+		</a>
+		<div class="lg:hidden">
+			<button class="navbar-burger flex items-center text-blue-600 p-3">
+				<svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<title>Mobile menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</button>
+		</div>
+		<ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+            @foreach($links as $key => $value)
+                <li>
+                    <a class="text-sm text-gray-400 hover:text-gray-500" href="{{ $value }}">
+                        {{ $key }}
+                    </a>
+                </li>
+                <li class="text-gray-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                    </svg>
+                </li>
+            @endforeach
+        </ul>
+
+		<a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Sign In</a>
+		<a class="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a>
+	</nav>
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// START HAMZA
+
+
+// END HAMZA
+
+
+// START NAWFAL
+
+
+
+// END NAWFAL
+
+
+// START IMAD
+
+
+// END IMAD
