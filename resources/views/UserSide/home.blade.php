0ea293d71d187e5f64284cda752077dee785180e
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-02-22 10:59:18 +0100

    navbar

--- resources/views/UserSide/home.blade.php
+++ resources/views/UserSide/home.blade.php
@@ -1,11 +1,11 @@
 @include('Shared.components.header')
 
 <body>
-<nav class="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-gray-800 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="../" class="h-8" alt="Flowbite Logo">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Anas</span>
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="logofinalnews.png" class="h-12" >
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
@@ -17,7 +17,7 @@
             </button>
         </div>
         <div class="hidden w-full md:flex md:w-auto md:order-1 items-center justify-between" id="navbar-sticky">
-            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
                 <li>
                     <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded hover:bg-blue-800 transition md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 md:hover:text-blue-600 focus:outline-none focus:ring focus:border-blue-300" aria-current="page">Home</a>
                 </li>
