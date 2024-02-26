5f34e31b317e700212a7280f596139fc2402736c
oussamaef06 <oussamaelfarihi@gmail.com>
2024-02-26 14:35:25 +0100

    fixing errors and updating model

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AdminSide\Category;
 use Illuminate\Http\Request;
-use App\Models\Category; // Assume you have a Category model
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        // Fetch categories from the database
+        
         $categories = Category::all();
         
-        // Pass categories to the view
+        
         return view('UserSide.categories', compact('categories'));
     }
 }
