753e58ee1c7e4466b32d3bdb64405af27f60a928
Naoufal <naoufal.labrihmi@gmail.com>
2024-02-28 11:11:36 +0100

    Update navbar.blade.php

--- resources/views/Shared/components/navbar.blade.php
+++ resources/views/Shared/components/navbar.blade.php
@@ -7,6 +7,8 @@
         z-index: 0;
     }
 
+
+
     .header-links li span::before {
         content: '';
         position: absolute;
@@ -27,7 +29,7 @@
     }
 </style>
 
-<header class="bg-white shadow-lg h-24 hidden md:flex">
+<header class="bg-gray-200 shadow-lg h-24 hidden md:flex fixed w-full z-1 top-0">
     <a href="" class="border flex-shrink-0 flex items-center justify-center px-4 lg:px-6 xl:px-8">
         <img class="" src="{{ URL::to('images/logo-removebg-preview.png') }}" alt="" />
     </a>
@@ -75,12 +77,7 @@
         </ul>
     </nav>
     <div class="border flex items-center px-4 lg:px-6 xl:px-8">
-        <a href="" class="mr-4 lg:mr-6 xl:mr-8">
-            <svg class="h-6 xl:h-8" aria-hidden="true" focusable="false" data-prefix="far" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-3x">
-                <path fill="currentColor" d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"></path>
-            </svg>
-        </a>
-        <button class="bg-black hover:bg-gray-700 text-white font-bold px-4 xl:px-6 py-2 xl:py-3 rounded">Contact Me</button>
+
     </div>
 </header>
 
